8a5fe02fa7f70df3ce78b92249a1ae6fea6dd10b
Jeffrey France <jbfrance1626@gmail.com>
2023-11-16 18:33:09 +0800

    remove tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#00a89a",
-          "secondary": "#009b00",
-          "accent": "#006900",
-          "neutral": "#2a2e33",
-          "base-100": "#2d2c20",
-          "info": "#61d5ff",
-          "success": "#00e5ba",
-          "warning": "#ff7400",
-          "error": "#ff2154",
-        },
-      },
-    ],
-  },
+  plugins: [require("daisyui")]
 }
 
